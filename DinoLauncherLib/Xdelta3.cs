58b9b52031681bbd14969e969d55856104e59e05
sabre230 <44850786+sabre230@users.noreply.github.com>
2022-10-27 10:47:07 -0700

    Added checks for operating system in Xdelta3 class

--- DinoLauncherLib/Xdelta3.cs
+++ DinoLauncherLib/Xdelta3.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace DinoLauncherLib;
@@ -10,42 +12,63 @@ public static class Xdelta3
     public static Task<string> ApplyPatch(FileIO fio, string fBaseRomPath, string fPatchPath, string fPatchedRomPath)
     {
         try
-        {
-            // Point to 'xdelta3.exe'
-            string exeDir = Path.Combine(fio.baseDir, fio.xdeltaPath);
-
-            // Specify arguments for procStartInfo()
-            // Added some escaped quotes to get around the spaces in paths issue
-            string args = $" -d -s \"{fBaseRomPath}\" \"{fPatchPath}\" \"{fPatchedRomPath}\"";
-
-            try
+        {            
+            #region Windows
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                ProcessStartInfo procStartInfo = new ProcessStartInfo
+                // Point to 'xdelta3.exe'
+                string exeDir = Path.Combine(fio.baseDir, fio.xdeltaPath);
+
+                // Specify arguments for procStartInfo()
+                // Added some escaped quotes to get around the spaces in paths issue
+                string args = $" -d -s \"{fBaseRomPath}\" \"{fPatchPath}\" \"{fPatchedRomPath}\"";
+
+                try
                 {
-                    FileName = exeDir,
-                    Arguments = args,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    WorkingDirectory = Path.Combine(fio.baseDir)
-                };
+                    ProcessStartInfo procStartInfo = new ProcessStartInfo
+                    {
+                        FileName = exeDir,
+                        Arguments = args,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        WorkingDirectory = Path.Combine(fio.baseDir)
+                    };
 
-                // Wait for the process to complete before exiting
-                using Process process = new Process();
-                process.StartInfo = procStartInfo;
-                process.Start();
+                    // Wait for the process to complete before exiting
+                    using Process process = new Process();
+                    process.StartInfo = procStartInfo;
+                    process.Start();
 
-                process.WaitForExit();
+                    process.WaitForExit();
 
-                Debug.WriteLine(process.StandardOutput.ReadToEnd());
-                Debug.WriteLine(exeDir + args);
+                    Debug.WriteLine(process.StandardOutput.ReadToEnd());
+                    Debug.WriteLine(exeDir + args);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("*** Error occured executing the following commands.");
+                    Debug.WriteLine("exeDir: " + exeDir);
+                    Debug.WriteLine("args: " + args);
+                    Debug.WriteLine("exception: " + ex.Message);
+                }
             }
-            catch (Exception ex)
+            #endregion
+            #region Linux
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                Debug.WriteLine("*** Error occured executing the following commands.");
-                Debug.WriteLine("exeDir: " + exeDir);
-                Debug.WriteLine("args: " + args);
-                Debug.WriteLine("exception: " + ex.Message);
+
+            }
+            #endregion
+            #region OSX
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+
+            }
+            #endregion
+            else
+            {
+                // OS isn't supported
             }
         }
         catch (Exception ex)
