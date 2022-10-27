5f25722d9342d02ab8b29d5eb7c00e50ad9852cd
sabre230 <44850786+sabre230@users.noreply.github.com>
2022-10-27 14:21:17 -0700

    Fixed word wrapping on main body text First try at getting xdelta3 to work on linux

---
+++ Build for Linux.bat
@@ -0,0 +1,2 @@
+dotnet publish -r linux-x64 --configuration release
+pause
--- DinoLauncherLib/Xdelta3.cs
+++ DinoLauncherLib/Xdelta3.cs
@@ -13,62 +13,43 @@ public static class Xdelta3
     {
         try
         {            
-            #region Windows
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            // Point to 'xdelta3.exe'
+            string exeDir = Path.Combine(fio.baseDir, fio.xdeltaPath);
+
+            // Specify arguments for procStartInfo()
+            // Added some escaped quotes to get around the spaces in paths issue
+            string args = $" -d -s \"{fBaseRomPath}\" \"{fPatchPath}\" \"{fPatchedRomPath}\"";
+
+            try
             {
-                // Point to 'xdelta3.exe'
-                string exeDir = Path.Combine(fio.baseDir, fio.xdeltaPath);
-
-                // Specify arguments for procStartInfo()
-                // Added some escaped quotes to get around the spaces in paths issue
-                string args = $" -d -s \"{fBaseRomPath}\" \"{fPatchPath}\" \"{fPatchedRomPath}\"";
-
-                try
+                ProcessStartInfo procStartInfo = new ProcessStartInfo
                 {
-                    ProcessStartInfo procStartInfo = new ProcessStartInfo
-                    {
-                        FileName = exeDir,
-                        Arguments = args,
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true,
-                        WorkingDirectory = Path.Combine(fio.baseDir)
-                    };
+                    // Trying something weird
+                    FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? exeDir : "xdelta3",
+                    //FileName = exeDir,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    WorkingDirectory = Path.Combine(fio.baseDir)
+                };
 
-                    // Wait for the process to complete before exiting
-                    using Process process = new Process();
-                    process.StartInfo = procStartInfo;
-                    process.Start();
+                // Wait for the process to complete before exiting
+                using Process process = new Process();
+                process.StartInfo = procStartInfo;
+                process.Start();
 
-                    process.WaitForExit();
+                process.WaitForExit();
 
-                    Debug.WriteLine(process.StandardOutput.ReadToEnd());
-                    Debug.WriteLine(exeDir + args);
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine("*** Error occured executing the following commands.");
-                    Debug.WriteLine("exeDir: " + exeDir);
-                    Debug.WriteLine("args: " + args);
-                    Debug.WriteLine("exception: " + ex.Message);
-                }
+                Debug.WriteLine(process.StandardOutput.ReadToEnd());
+                Debug.WriteLine(exeDir + args);
             }
-            #endregion
-            #region Linux
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            catch (Exception ex)
             {
-
-            }
-            #endregion
-            #region OSX
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-
-            }
-            #endregion
-            else
-            {
-                // OS isn't supported
+                Debug.WriteLine("*** Error occured executing the following commands.");
+                Debug.WriteLine("exeDir: " + exeDir);
+                Debug.WriteLine("args: " + args);
+                Debug.WriteLine("exception: " + ex.Message);
             }
         }
         catch (Exception ex)
